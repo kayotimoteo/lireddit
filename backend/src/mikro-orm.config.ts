d2f5cd19a5ea411c258914cf5cab7e6ea6ad9a3b
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-09-01 23:15:11 -0400

    Create Session

---
+++ backend/src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from '@mikro-orm/core'
+import path from "path"
+import { User } from "./entities/User";
+
+console.log("dirname: ", __dirname)
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post, User],
+  dbName: 'libreddit',
+  user: 'postgres',
+  password: '12345678',
+  type: 'postgresql',
+  debug:!__prod__,
+} as Parameters<typeof MikroORM.init>[0];
