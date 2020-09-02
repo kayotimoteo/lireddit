417d545f6a4ee62a2e3c5395c322d0cc3d5ed8db
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-09-02 00:10:50 -0400

    Create page Regiser

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/core'
+
+const Index = () => (
+  <div>Hello World</div>
+)
+
+export default Index
---
+++ web/src/pages/register.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Formik, Form } from 'formik'
+import { Box, Button } from '@chakra-ui/core';
+import Wrapper from '../components/Wrapper';
+import InputField from '../components/InputField';
+
+// import { Container } from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{username: "", password: ""}}
+        onSubmit={values => {console.log(values)}}
+      >
+        {({isSubmitting}) => (
+          <Form>
+            <InputField 
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField 
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button 
+              mt={4} 
+              type="submit" 
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+}
+
+export default Register;
